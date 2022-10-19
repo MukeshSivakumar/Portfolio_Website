eb946773541e8c8d3cc1a0ee170893825a2ef7e3
MukeshSivakumar <mukeshsivakumar97@gmail.com>
2022-10-20 00:34:46 +0530

    Portfolio website files

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+function changeDisplay(sectionName,elementID)
+{
+    var sectionArray = ["skill-section","experience-section","education-section","certificate-section"];
+    var elementArray = ["skills","experience","education","certificate"];
+    for(x in sectionArray,elementArray)
+    {
+        
+         if(sectionName == sectionArray[x])
+        {
+            let ele = document.getElementById(sectionArray[x]);
+            ele.style.display = "block";
+            let element = document.getElementById(elementArray[x]);
+            element.classList.add("active");
+        }
+        else{
+            let ele = document.getElementById(sectionArray[x]);
+            ele.style.display = "none";
+            let element = document.getElementById(elementArray[x]);
+            element.classList.remove("active");
+        }
+        
+    }
+}
+
+function changebg(event,mobileEle){
+    let mob = document.getElementById(mobileEle);
+    if(event == "over")
+    {
+        mob.style.backgroundColor = "#ff004f";
+        mob.style.transitionDuration = "0.5s";
+    }else {
+        mob.style.backgroundColor = "#262626";
+    }
+    
+}
